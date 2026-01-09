7366d8ac2a82976445d372b0c682fca9d213d401
abin-007 <sebumappu@gmail.com>
2026-01-09 23:20:18 +0530

    mid was not calculated inside the while loop , corrected the binary search solution

--- Leetcode/BinarySearchSolution.java
+++ Leetcode/BinarySearchSolution.java
@@ -8,8 +8,9 @@ public class BinarySearchSolution {
     public static int search(int[] nums, int target){
         int low=0;
         int high= nums.length-1;
-        int mid=low +(high-low)/2;
+        
         while(low<=high){
+            int mid=low +(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
