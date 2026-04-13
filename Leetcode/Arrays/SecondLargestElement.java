0240e23f802e545030ec3357714f19f8db9e6610
abin-007 <sebumappu@gmail.com>
2026-04-13 21:18:52 +0530

    array problem - getsecond largest using the two pass method

--- Leetcode/Arrays/SecondLargestElement.java
+++ Leetcode/Arrays/SecondLargestElement.java
@@ -1,21 +1,42 @@
 package Arrays;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class SecondLargestElement {
-    ///naive approach using the sorting method
+    
     static int getSecondLargest(int[] arr){
         int n=arr.length;
-        // sorting the array in increasing order       
-         Arrays.sort(arr);
+        
+        ///naive approach using the sorting method
 
-         for(int i= n-2;i>=0;i--){
-            if(arr[i]!=arr[n-1]){
-                return arr[i];
-            }
+        // sorting the array in increasing order       
+        //  Arrays.sort(arr);
+
+        //  for(int i= n-2;i>=0;i--){
+        //     if(arr[i]!=arr[n-1]){
+        //         return arr[i];
+        //     }
            
-         }
-          return -1;
+        //  }
+        //   return -1;
+
+        /// better approach using the two pass method
+        int largest = -1; 
+        int secondLargest=-1;
+        // loop to find the largest element
+        for(int i =0; i<n;i++){
+            if (arr[i]> largest){
+                largest= arr[i];
+            }
+        }
+
+        // loop for finding the second largest
+        for(int i =0; i<n;i++){
+            if (arr[i]> secondLargest && arr[i]!=largest){
+                secondLargest= arr[i];
+            }
+        }
+        return secondLargest;
     }
     public static void main(String args[]){
         int[] arr={10, 2,30, 35,15,21};
