83e5ae9de169f77162fdbea3ccaf9743d1d54c13
abin <sebumappu@gmail.com>
2026-06-01 22:57:15 +0530

    Container with max amount of water using array and two pointer

---
+++ Leetcode/Arrays/ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height){
+        int left =0;
+        int right = height.length-1;
+        int maxArea =0;
+        while(left<right){
+            int width = right - left;
+            int currentArea =Math.min(height[left],height[right])*width;
+            maxArea = Math.max(maxArea, currentArea);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right --;
+            }
+
+        }
+        return maxArea;
+    }
+    public static void main(String a[]){
+        int [] height ={1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
